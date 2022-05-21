25ed89fdca00acc2bc621b4c89ac944e6ea386d8
Alfredo Hernandez Noriega <alfredo.hernandez@kushkipagos.com>
2022-05-21 13:12:56 -0500

    ejemplo rutas

--- src/App.js
+++ src/App.js
@@ -2,30 +2,15 @@ import React, { useState, useEffect } from 'react'
 import logo from './logo.svg';
 import axios from 'axios';
 import './App.css';
-import CardQuote from './components/CardQuote';
 
 function App() {
 
-  const [quote, setQuote] = useState({})
 
-  const getQuotes = async () => {
-    const url = 'https://breakingbadapi.com/api/quote/random'
-    const resp = await axios.get(url)
-    setQuote(resp.data[0])
-
-    console.log(resp.data[0])
-  }
-  useEffect(() => {
-    getQuotes()
-  }, [])
 
   return (
     <div className="App">
       <header className="App-header">
-        <CardQuote info={quote} />
-        <button onClick={() => getQuotes()} >
-          Click
-        </button>
+
       </header>
     </div>
   );
